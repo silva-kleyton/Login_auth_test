466a13a81efe2cf6df7bda540ba8abbb7f620459
Kleyton Silva <KNascimento@MacBook-Pro-de-user.local>
2020-03-09 10:00:53 -0300

    develop2

--- __tests__/integration/user.test.js
+++ __tests__/integration/user.test.js
@@ -1,7 +1,14 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../../__tests__utils/truncate';
+
 describe('User', () => {
+  // clear database before it's
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to registrer', async () => {
     const response = await request(app)
       .post('/users')
@@ -12,5 +19,25 @@ describe('User', () => {
       });
 
     expect(response.body).toHaveProperty('id');
+  });
+
+  it('should not be able to register with duplicated email', async () => {
+    await request(app)
+      .post('/users')
+      .send({
+        name: 'Kleyton',
+        email: 'kleyton@teste.com.br',
+        password: '123456',
+      });
+
+    const response = await request(app)
+      .post('/users')
+      .send({
+        name: 'Kleyton',
+        email: 'kleyton@teste.com.br',
+        password: '123456',
+      });
+
+    expect(response.status).toBe(400);
   });
 });
--- src/app/controllers/User.js
+++ src/app/controllers/User.js
@@ -3,6 +3,11 @@ import User from '../models/User';
 class UserController {
   async store(req, res) {
     try {
+      const { email } = req.body;
+
+      if (await User.findOne({ where: { email } }))
+        return res.status(400).json({ error: 'Email user exists' });
+
       const user = await User.create(req.body);
 
       return res.status(200).json(user);
